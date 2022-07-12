bc80677ed2a5029476d6035a22f6b52c24b34a48
Anara Akmatalieva <anara.bakyt@gmail.com>
2022-07-13 01:39:10 +0600

    todo list

---
+++ src/components/model.ts
@@ -0,0 +1,26 @@
+export interface Todo{
+    id:number;
+    todo:string;
+    isDone:boolean
+}
+// type Actions=
+// |{ type:'add', payload:string} 
+// |{type:'remove', payload:number};
+// |{type:'done', payload:number}
+// const TodoReducer=(state:Todo[],action)=>{
+// switch(action.type){
+
+// }
+// }
+
+
+// import React,{useReducer} from 'react'
+
+// const ReducerExample = () => {
+//     const [state, dispatch] = useReducer(TodoReducer, [])
+//   return (
+//     <div></div>
+   
+//   )
+// }
+
